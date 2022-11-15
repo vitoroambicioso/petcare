5d1cfd13a6883bda35fb40da597602c8d0787990
joaovitorazerd <isidorojoao21@gmail.com>
2022-11-15 13:34:50 -0300

    disable cors x-csrf-token

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -20,7 +20,7 @@ class Cors
         //ALLOW OPTIONS METHOD
         $headers = [
             'Access-Control-Allow-Methods' => 'POST,GET,OPTIONS,PUT,DELETE',
-            'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization',
+            'Access-Control-Allow-Headers' => 'Content-Type, X-CSRF-TOKEN, Origin, Authorization',
         ];
         if ($request->getMethod() == "OPTIONS"){
             //The client-side application can set only headers allowed in Access-Control-Allow-Headers
