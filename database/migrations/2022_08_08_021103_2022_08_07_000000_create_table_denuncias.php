971eed3507df2d8db2bcb6518b806641809e4fba
joaovitorazerd <isidorojoao21@gmail.com>
2022-11-15 18:04:05 -0300

    update migrations

--- database/migrations/2022_08_08_021103_2022_08_07_000000_create_table_denuncias.php
+++ database/migrations/2022_08_08_021103_2022_08_07_000000_create_table_denuncias.php
@@ -24,6 +24,7 @@ return new class extends Migration
             $table->string('bairro');
             $table->string('pontoDeReferencia');
             $table->string('picture');
+            $table->string('descricao');
             $table->timestamps();
         });
 
