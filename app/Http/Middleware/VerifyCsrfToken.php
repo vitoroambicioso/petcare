278e88fbfc0debc0f7287a8e983888ad0336b162
joaovitorazerd <isidorojoao21@gmail.com>
2022-11-15 17:05:16 -0300

    except verifytoken origin

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        'https://pet-care-front-end-.vercel.app'
+        'https://pet-care-front-end-.vercel.app/*'
     ];
 }
