92ad5b3a10034f1c6a14d4e142a3d3d0da914318
joaovitorazerd <isidorojoao21@gmail.com>
2022-11-23 15:16:45 -0300

    picture de string para text

--- database/migrations/2022_08_08_021103_2022_08_07_000000_create_table_denuncias.php
+++ database/migrations/2022_08_08_021103_2022_08_07_000000_create_table_denuncias.php
@@ -23,7 +23,7 @@ return new class extends Migration
             $table->string('rua');
             $table->string('bairro');
             $table->string('pontoDeReferencia');
-            $table->string('picture');
+            $table->text('picture');
             $table->string('descricao');
             $table->timestamps();
         });
