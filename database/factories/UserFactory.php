58351101672b63f4a3073f4443e7a20588eb6eca
joaovitorazerd <isidorojoao21@gmail.com>
2022-11-29 18:11:12 -0300

    salvar foto de perfil do usuario

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,6 +20,7 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->safeEmail(),
+            'photo' => fake()->name()."png",
             'email_verified_at' => now(),
             'password' => 'password', // password
             'remember_token' => Str::random(10),
