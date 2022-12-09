70269f08db7d4562b2c5294918737f7eec7fb074
joaovitorazerd <isidorojoao21@gmail.com>
2022-12-09 18:48:24 -0300

    retirar rotas do grupo middleware web

--- routes/web.php
+++ routes/web.php
@@ -23,26 +23,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['middleware' => ['web']], function () {
-    /**
-     * rotas do usuario
-     */
-    Route::post('/usuario', [UserController::class, 'create'])->name('user.create');
-    Route::post('/usuario/{id}', [UserController::class, 'getUser'])->name('user.get');
-    Route::put('/usuario/{id}', [UserController::class, 'edit'])->name('user.edit');
-    Route::delete('/usuario/{id}', [UserController::class, 'delete'])->name('user.delete');
-    Route::get('/usuarios', [UserController::class, 'getAllUsers']);
-    Route::post('/login', [UserController::class, 'login'])->name('user.login');
+/**
+ * rotas do usuario
+ */
+Route::post('/usuario', [UserController::class, 'create'])->name('user.create');
+Route::post('/usuario/{id}', [UserController::class, 'getUser'])->name('user.get');
+Route::put('/usuario/{id}', [UserController::class, 'edit'])->name('user.edit');
+Route::delete('/usuario/{id}', [UserController::class, 'delete'])->name('user.delete');
+Route::get('/usuarios', [UserController::class, 'getAllUsers']);
+Route::post('/login', [UserController::class, 'login'])->name('user.login');
 
-    /**
-     * rotas da denuncia
-     */
-    Route::post('/denuncia', [DenunciaController::class, 'create'])->name('denuncia.create');
-    Route::post('/denuncias', [DenunciaController::class, 'getDenuncia'])->name('denuncia.get');
-    Route::put('/denuncia/{id}', [DenunciaController::class, 'edit'])->name('denuncia.edit');
-    Route::delete('/denuncia/{id}', [DenunciaController::class, 'delete'])->name('denuncia.delete');
-    Route::get('/denuncias', [DenunciaController::class, 'getAllDenuncias']);
-});
+/**
+ * rotas da denuncia
+ */
+Route::post('/denuncia', [DenunciaController::class, 'create'])->name('denuncia.create');
+Route::post('/denuncias', [DenunciaController::class, 'getDenuncia'])->name('denuncia.get');
+Route::put('/denuncia/{id}', [DenunciaController::class, 'edit'])->name('denuncia.edit');
+Route::delete('/denuncia/{id}', [DenunciaController::class, 'delete'])->name('denuncia.delete');
+Route::get('/denuncias', [DenunciaController::class, 'getAllDenuncias']);
 
 /**
  * gerando token CSRF
